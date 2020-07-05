b896068ac2cc873db5f5ff5ea4c37d0d8cb634e7
Yerjanik Arakel <arakelyanyerjanik@gmail.com>
2020-07-05 15:25:57 +0400

    start work with parcer bundler

--- static/gulpFile.js
+++ static/gulpFile.js
@@ -1,1 +1,26 @@
-let browserSync = require("browser-sync");
+const gulp = require("gulp");
+
+let browserSync = require("browser-sync").create();
+
+function sync(done) {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+    port: 3030,
+  });
+
+  done();
+}
+
+function browserReload(done) {
+  browserSync.reload();
+  done();
+}
+
+function watchFiles() {
+  gulp.watch("./**/*.html", browserReload);
+}
+
+gulp.task("default", gulp.parallel(watchFiles, sync));
+// gulp.task(sync);
